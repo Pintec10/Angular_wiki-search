a826633a2a23197f75b6ac358e42c6ff8e57e74d
Roberto Milani <milani@strands.com>
2021-11-09 17:24:02 +0100

    added typing and rxjs pipe to http response

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,27 @@
 import { Component } from '@angular/core';
 import { WikipediaService } from './wikipedia.service';
 
+interface WikiPage {
+  title: string,
+  snippet: string,
+  pageid: number
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  pages = [];
+  pages: WikiPage[] = [];
 
   constructor(
     private wikipedia: WikipediaService
   ) { }
 
   onTermSubmitted(term: string) {
-    this.wikipedia.search(term).subscribe((response: any) => {
-      this.pages = response.query.search;
+    this.wikipedia.search(term).subscribe((data) => {
+      this.pages = data;
     });
   }
 }
--- src/app/wikipedia.service.ts
+++ src/app/wikipedia.service.ts
@@ -1,5 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { pluck } from 'rxjs/operators';
+
+interface WikipediaResponse {
+  query: {
+    search: {
+      title: string,
+      snippet: string,
+      pageid: number
+    }[];
+  };
+}
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +22,7 @@ export class WikipediaService {
   ) { }
 
   search(term: string) {
-    return this.http.get('https://en.wikipedia.org/w/api.php', {
+    return this.http.get<WikipediaResponse>('https://en.wikipedia.org/w/api.php', {
       params: {
         action: 'query',
         format: 'json',
@@ -20,6 +31,8 @@ export class WikipediaService {
         srsearch: term,
         origin: '*'
       }
-    });
+    }).pipe(
+      pluck('query', 'search')
+    );
   }
 }
